7f2a5cacfca947aabe3b91850f85dbb08cda6808
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-03-31 20:33:16 +0700

    bai tap chuong 5

---
+++ IT002O216/Chuong5/BaiTap.cpp
@@ -0,0 +1,47 @@
+// 1. Cài đặt lớp số phức để biểu diễn khái niệm số phức, cho phép quan điểm một số thực như một số phức đặc biệt(phần ảo bằng 0).
+// ▪ Định nghĩa các phép toán +, -, *, /, ==, <<, >> trên số phức.
+// ▪ Định nghĩa các thao tác cần thiết cho lớp số phức
+
+// 2. Giả sử lớp PhanSo đã được khai báo.
+// ▪ Cài đặt toán tử > như khai báo cho lớp PhanSo
+// ▪ Hãy xây dựng lớp DSPhanSo để lưu trữ một
+// mảng các phân số cho phép thực hiện các thao
+// tác sau :
+// ▪ Nhập giá trị cho các phân số trong mảng
+// ▪ Tìm phân số có giá trị lớn nhất.
+// ▪ Tính tổng các phân số trong mảng
+// ▪ Hãy viết chương trình để nhập một mảng
+//   gồm n phân số(n do người dùng nhập)
+// ▪ tìm và in ra màn hình phân số có giá trị lớn nhất.
+
+// 3. Xét đa thức theo biến x (đa thức một biến) bậc n có dạng như sau:
+//
+//        P(X) = a1.x^(n) + a2.x^(n-1) + a3.x^(n-2)+ … + aj
+//
+//    Trong đó: n là bậc của đa thức. a1, a2 , a3, …, aj
+//    là các hệ số tương ứng với từng bậc của đa thức.
+//    Xây dựng lớp DaThuc biểu diễn khái niệm đa thức với các thao tác sau:
+// ▪ Khởi tạo một đa thức có bậc bằng 0 hoặc bậc n.
+//// check ✓
+// ▪ Tính giá trị của đa thức khi biết giá trị của x
+//// check ✓
+// ▪ Nhập đa thức
+//// check ✓
+// ▪ Xuất đa thức
+//// check ✓
+// ▪ Định nghĩa các toán tử: >>, <<, +, -
+//// check ✓
+// ▪ Viết chương trình cho phép người dùng nhập vào hai đa thức rồi xuất các
+//   đa thức ra màn hình. Sau đó tính tổng hai đa thức và xuất kết quả ra màn
+//   hình.
+//// check ✓
+
+// 4. Một ma trận là một mảng hai chiều gồm m dòng
+//    và n cột. Hãy khai báo và cài đặt lớp biểu diễn
+//    khái niệm ma trận để thực hiện các yêu cầu sau:
+// ▪ Tạo ma trận ngẫu nhiên m dòng, n cột
+// ▪ Nhập ma trận m dòng n cột
+// ▪ Operator cộng hai ma trận
+// ▪ Operator nhân hai ma trận
+// ▪ Viết chương trình cho phép tạo 2 ma trận, cộng,
+//   trừ 2 ma trận và xuất kết quả ra màn hình.
