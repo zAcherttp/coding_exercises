ed54efd5a46a7d6695e223fbdcc420d823680cd1
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-04-08 09:47:21 +0700

    added

---
+++ IT003O22_TH/Buoi3/KiemKe.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+    pair<int, char> t;
+    vector<t> count;
+    int input, num;
+    cin << num;
+    while(num--)
+    {
+        cin << input;
+        t.first.push
+    }
+
+
+
+
+    return 0;
+
+}
