a6446775c6f9f355b144b1ae4975c12be9589eed
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-04-09 07:37:31 +0700

    added

---
+++ IT003O22_TH/Buoi3/DSLK_PrintList.cpp
@@ -0,0 +1,101 @@
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+class SinglyLinkedListNode
+{
+public:
+    int data;
+    SinglyLinkedListNode *next;
+
+    SinglyLinkedListNode(int node_data)
+    {
+        this->data = node_data;
+        this->next = nullptr;
+    }
+};
+
+class SinglyLinkedList
+{
+public:
+    SinglyLinkedListNode *head;
+    SinglyLinkedListNode *tail;
+
+    SinglyLinkedList()
+    {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void insert_node(int node_data)
+    {
+        SinglyLinkedListNode *node = new SinglyLinkedListNode(node_data);
+
+        if (!this->head)
+        {
+            this->head = node;
+        }
+        else
+        {
+            this->tail->next = node;
+        }
+
+        this->tail = node;
+    }
+    // Complete the printLinkedList function below.
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode* next;
+     * };
+     *
+     */
+    void printLinkedList()
+    {
+
+        SinglyLinkedListNode* current = this->head;
+        while(current != nullptr)
+        {
+            cout << current->data << '\n';
+            current = current->next;
+        }
+
+    }
+};
+
+void free_singly_linked_list(SinglyLinkedListNode *node)
+{
+    while (node)
+    {
+        SinglyLinkedListNode *temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+int main()
+{
+    SinglyLinkedList *llist = new SinglyLinkedList();
+    int llist_count;
+    int x;
+
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++)
+    {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+    llist->printLinkedList();
+    free_singly_linked_list(llist->head);
+
+    return 0;
+}
