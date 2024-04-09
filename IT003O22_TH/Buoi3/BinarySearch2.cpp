d69233cf02dfa1dd220359a2644445728632052c
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-04-09 09:19:58 +0700

    Changes

--- IT003O22_TH/Buoi3/BinarySearch2.cpp
+++ IT003O22_TH/Buoi3/BinarySearch2.cpp
@@ -2,36 +2,3 @@
 
 using namespace std;
 
-class element
-{
-public:
-    int value;
-    int l_pos;
-    int h_pos;
-
-    element(int value = 0)
-    {
-        l_pos = -1;
-        h_pos = -1;
-    }
-
-    friend istream &operator>>(istream &is, element &e)
-    {
-        is >> e.value;
-        return is;
-    }
-};
-
-int main()
-{
-    int N, Q;
-    cin >> N;
-    element arr[N];
-
-    for(int i = 0; i < N; i++)
-    {
-        cin >> arr[i];
-        if(arr[i].l_pos == -1) arr[i].l_pos = i;
-        else arr[i].h_pos = i;
-    }
-}
