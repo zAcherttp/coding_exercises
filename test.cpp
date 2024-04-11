21d98e489638c7f27b5ff8a893f0bd07f59b68aa
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-04-11 16:11:05 +0700

    changes

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <set>
 using namespace std;
 class IntSet
 {
@@ -11,13 +12,13 @@ public:
 	IntSet(int *values = nullptr, int count = 0) {}
 	~IntSet() {}
 
-	int* setPtr() {}
-	int getCount() {}
-
+	void setSet(int *values, int count);
+	int getCount() const;
+	void makeSet();
 	friend istream &operator>>(istream &is, IntSet &set);
 	friend ostream &operator<<(ostream &os, const IntSet &set);
 
-	int& operator[](int index);
+	int &operator[](int index);
 	IntSet operator+(const IntSet &set);
 	IntSet operator-(const IntSet &set);
 	bool operator==(const IntSet &set);
@@ -25,7 +26,7 @@ public:
 
 IntSet::IntSet(int *_values = nullptr, int _count = 0)
 {
-	if(_values == nullptr)
+	if (_values == nullptr)
 		values = new int[_count];
 	else
 		count = _count;
@@ -35,13 +36,25 @@ IntSet::~IntSet()
 	delete[] values;
 }
 
-int* IntSet::setPtr()
-{
-	return values;
-}
-int IntSet::getCount()
+int IntSet::getCount() const
 {
 	return count;
+}
+void IntSet::makeSet()
+{
+	set<int> set;
+	for(int i = 0; i < count; i++)
+	{
+		set.insert(values[i]);
+	}
+	count = set.size();
+	delete[] values;
+	values = new int[count];
+	int i = 0;
+	for(auto value : set)
+	{
+		values[i++] = value;
+	}
 }
 
 istream &operator>>(istream &is, IntSet &set)
@@ -54,48 +67,44 @@ istream &operator>>(istream &is, IntSet &set)
 }
 ostream &operator<<(ostream &os, const IntSet &set)
 {
-	for(int i = 0; i < set.count - 1; i++)
+	for (int i = 0; i < set.count - 1; i++)
 	{
 		os << set.values[i] << " ";
 	}
 	os << set.values[set.count - 1];
 	return os;
 }
-
-int& IntSet::operator[](int index)
+int &IntSet::operator[](int index)
 {
-	if(index >= count || index < 0)
+	if (index >= count || index < 0)
 		cout << "index out of bound";
-		exit(0);
+	exit(0);
 	return values[index];
 }
 
 IntSet IntSet::operator+(const IntSet &set)
 {
 	IntSet _union;
-	IntSet _sorted1 = *this;
+	this->makeSet();
+	set
+	IntSet _sorted1 = *this->makeSet();
 	IntSet _sorted2 = set;
 
 	sort(_sorted1.values, _sorted1.values + _sorted1.count);
 	sort(_sorted2.values, _sorted2.values + _sorted2.count);
 
-
-
 	_union.count = this->count + set.count;
-
 }
 IntSet IntSet::operator-(const IntSet &set)
 {
-
 }
 bool IntSet::operator==(const IntSet &set)
 {
-
 }
 
 int main()
 {
-	int* list = new int[3];
+	int *list = new int[3];
 	list[0] = 1;
 	list[1] = 2;
 	list[2] = 2;
