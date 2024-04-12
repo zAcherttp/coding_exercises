f2b1a71078c847f72efc02accfd6f85d098715ef
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-04-12 21:15:58 +0700

    changes

--- IT003O22_TH/Buoi3/KiemKe2.cpp
+++ IT003O22_TH/Buoi3/KiemKe2.cpp
@@ -1,26 +1,84 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Data
 {
 public:
-    string key;
+    int key;
     int count;
-bool operator<(const Data &d)
-{
-    return count == d.count ? (key > d.key) : (count < d.count);
-}
 };
+
+bool cmp(const Data &a, const Data &b)
+{
+    return a.count == b.count ? (a.key > b.key) : (a.count < b.count);
+}
+
+int partition(vector<Data> &arr, int low, int high)
+{
+    Data pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; ++j)
+    {
+        if (cmp(arr[j], pivot))
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quick(vector<Data> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quick(arr, low, pi - 1);
+        quick(arr, pi + 1, high);
+    }
+}
 
 int main()
 {
     int n;
     cin >> n;
-    Data DS[n];
-    
 
+    vector<int> maHangs(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> maHangs[i];
+    }
+
+    vector<Data> banDem;
+    for (int maHang : maHangs)
+    {
+        bool found = false;
+        for (auto &pair : banDem)
+        {
+            if (pair.key == maHang)
+            {
+                pair.count++;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            banDem.push_back({maHang, 1});
+        }
+    }
+
+    quick(banDem, 0, banDem.size() - 1);
+    reverse(banDem.begin(), banDem.end());
+
+    for (auto pair : banDem)
+    {
+        cout << pair.key << " " << pair.count << "\n";
+    }
 
     return 0;
 }
--- IT003O22_TH/Buoi3/tempCodeRunnerFile.cpp
+++ IT003O22_TH/Buoi3/tempCodeRunnerFile.cpp
@@ -1,24 +1,3 @@
-    SinglyLinkedListNode* current = head;
-    SinglyLinkedListNode* element = new SinglyLinkedListNode(x);
-
-    if(current->next->data > x)
-    {
-        element->next = head;
-        head = element;
-        return head;
-    }
-    while(current != nullptr)
-    {
-        if(current->next->data > x)
-        {
-            element->next = current->next;
-            current->next = element;
-            return head;
-        }
-        current = current->next;
-    }
-    if(current == nullptr && current->data < x)
-    {
-        current->next = element;
-    }
-    return head;
+    // for (auto i : data)
+    // {
+    // }
