c7e72ecf5ffac8474f3fdd05066581994c0110a8
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-05-04 08:32:08 +0700

    changes

---
+++ IT002O216/Chuong7/BaiTap.cpp
@@ -0,0 +1,22 @@
+// ❖ Bài toán: Công ty ABC là công ty sản xuất kinh doanh thú nhồi
+// bông. Công ty có nhiều nhân viên làm việc trong ba bộ phận khác
+// nhau: bộ phận quản lý, bộ phận sản xuất, bộ phận văn phòng.
+// Việc tính lương cho nhân viên dựa vào các yếu tố sau:
+// ▪ Đối với nhân viên văn phòng: Lương = Lương Cơ Bản + Số ngày làm
+// việc *200.000 + Trợ Cấp
+// ▪ Đối với nhân viên sản xuất: Lương = Lương Cơ Bản + Số Sản Phẩm *
+// 2.000
+// ▪ Đối với nhân viên quản lý: Lương = Lương Cơ Bản* Hệ số chức vụ +
+// Thưởng.
+// ❖ Ngoài ra công ty cần quản lý các thông tin về nhân viên của mình
+// như: họ tên, ngày sinh và các thông số trên để tính lương cho
+// từng nhân viên trong công ty.
+
+// ❖Yêu cầu: Thiết kế các lớp thích hợp để thực hiện
+// các yêu cầu sau:
+// ▪ Nhập thông tin của các nhân viên để phục vụ cho việc
+// tính lương.
+// ▪ Thực hiện việc tính lương cho từng nhân viên.
+// ▪ Xuất thông tin của các nhân viên.
+// ▪ Tính tổng lương của công ty.
+// ▪ Tìm kiếm một nhân viên theo họ tên.
