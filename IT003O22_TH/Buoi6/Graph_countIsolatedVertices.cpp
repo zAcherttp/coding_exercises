94fd7ba1fb5deb761c3cd97b46b6a000f147fb58
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-05-25 14:40:01 +0700

    removed /vstudio

--- IT003O22_TH/Buoi6/Graph_countIsolatedVertices.cpp
+++ IT003O22_TH/Buoi6/Graph_countIsolatedVertices.cpp
@@ -7,16 +7,15 @@ int main() {
     unordered_map<int, int> edges;
     cin >> m >> n;
     n *= 2;
-    while(n--)
-    {
+    while (n--) {
         int temp;
         cin >> temp;
         edges[temp]++;
     }
 
-    while(m--)
-    {
-        if(edges[m] == 0) count++;
+    while (m--) {
+        if (edges[m] == 0)
+            count++;
     }
     cout << count;
     return 0;
