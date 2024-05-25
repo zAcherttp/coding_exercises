044c85dc6d8f7361b90736dc1ad7cc4c36dfd29f
zAcherttp <60420569+zAcherttp@users.noreply.github.com>
2024-05-25 14:13:55 +0700

    graph exercises

--- IT003O22_TH/Buoi6/Graph_countConnectedGraph.cpp
+++ IT003O22_TH/Buoi6/Graph_countConnectedGraph.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    
-    int v, e;
-    cin >> v, e;
-    vector<vector<bool>> graph(v, vector<bool>(v, false));
+    int vertice, edge;
+    cin >> vertice >> edge;
 
-    int v1, v2;
-    for(int i = 0; i < e; i++) {
-        cin >> v1 >> v2;
-        graph[v1][v2] = true;
-        graph[v2][v1] = true;
+    vector<vector<char>> graph(vertice, vector<char>(vertice, '-'));
+
+    for (int v = 0; v < vertice; v++) {
+        graph[v][v] = to_string(v)[0];
     }
 
+    for (int e = 0; e < edge; e++) {
+        int v1, v2;
+        cin >> v1 >> v2;
+        graph[v1][v2] = 'O';
+        graph[v2][v1] = 'O';
+    }
+
+    int connectedGraphCount = 0;
+    vector<bool> visited(vertice, false);
+
+    for (int v = 0; v < vertice; v++) {
+        if (!visited[v]) {
+            connectedGraphCount++;
+            queue<int> nodeQueue;
+            nodeQueue.push(v);
+            visited[v] = true;
+
+            while (!nodeQueue.empty()) {
+                int currentNode = nodeQueue.front();
+                nodeQueue.pop();
+
+                for (int neighbor = 0; neighbor < vertice; neighbor++) {
+                    if (graph[currentNode][neighbor] == 'O' && !visited[neighbor]) {
+                        nodeQueue.push(neighbor);
+                        visited[neighbor] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << connectedGraphCount;
 
     return 0;
 }
